a262b7ebfc17b95b2e452fa56917987b538c7660
Christi Kerr <christinakerr394@gmail.com>
2020-11-26 11:12:06 -0600

    Added book controller

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     title: { type: String, required: true },
     author: {type:String, required: true },
-    synopsis: {type: String, require: true },
+    description: {type: String, require: true },
     image: {type: String, require: true },
     link: {type: String, require: true },
 });
